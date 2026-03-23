058f40338b69f732a7013187a35c0e22c5b562df
Elton Luiz <eltonluiz533@gmail.com>
2026-03-23 00:54:22 -0300

    feat: Cria a regras iniciais de aprimoramento e contador de vidas do jogador.

--- main.py
+++ main.py
@@ -5,12 +5,27 @@ import random
 import sound
 
 from enemy import Enemy
+from improvement import Improvement
 from player import Player
 from shot import Shot
+from textSprite import TextSprite
+
+# Variáveis
+gameLoop = True
+
+timer = 20
+
+remainingLifes = 1
+
+numberShots = 1
+
+clock = pygame.time.Clock()
+
+textRect = pygame.Rect(51,15,50,50)
 
 # Inicializando o Pygame e criando a Janela
 pygame.init()
-display = pygame.display.set_mode([800,600])
+display = pygame.display.set_mode([800, 600])
 pygame.display.set_caption("ShotSpace")
 icon = pygame.image.load("data/images/Rocket.png")
 pygame.display.set_icon(icon)
@@ -18,41 +33,42 @@ pygame.display.set_icon(icon)
 # Groups
 objectGroup = pygame.sprite.Group()
 enemyGroup = pygame.sprite.Group()
+improvementGroup = pygame.sprite.Group()
 shotGroup = pygame.sprite.Group()
 
 # Background
 bg = pygame.sprite.Sprite(objectGroup)
 bg.image = pygame.image.load("data/images/bgSpace.png")
-bg.image = pygame.transform.scale(bg.image, (800,600))
+bg.image = pygame.transform.scale(bg.image, (800, 600))
 bg.rect = bg.image.get_rect()
 
 # Objects
-playerRect = pygame.Rect(400,450,100,100);
-player = Player(objectGroup,"data/images/Rocket.png", playerRect,1)
+player = Player(objectGroup, "data/images/Rocket.png", 1)
 sounds = sound.Sound()
+
+# Sprite de vida restante
+lifes = pygame.sprite.Sprite(objectGroup)
+lifes.image = pygame.image.load("data/images/life.png")
+lifes.image = pygame.transform.scale(lifes.image, (50, 50))
+lifes.rect = pygame.Rect(0, 0, 50, 50)
+
+quantityLifes = TextSprite(f": {remainingLifes}", 40, (35, 176, 76), textRect, objectGroup)
 
 # Cria função com estilização do jogo
 def draw():
-    display.fill([45, 45, 45])
-
     objectGroup.draw(display)
 
-# Variáveis
-gameLoop = True
-timer = 20
-clock = pygame.time.Clock()
 
 # Atualiza tela para continuar aberta
 if __name__ == '__main__':
     while gameLoop:
         clock.tick(60)
 
-        # Evento de fechar o game
+        draw()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 gameLoop = False
-
-            # Evento de tiro do jogador
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     sounds.play('shot')
@@ -61,25 +77,41 @@ if __name__ == '__main__':
 
         objectGroup.update()
 
-        # Geração aleatória de inimigos com 30% de chance
         timer += 1
         if timer > 30:
             timer = 0
             if random.random() < 0.3:
                 newEnemy = Enemy("data/images/Asteroid.png", objectGroup, enemyGroup)
 
+            if random.random() < 0.1:
+                if random.random() < 0.5:
+                    newImprovement = Improvement("data/images/moreShot.png", 'shot', objectGroup, improvementGroup)
+                else:
+                    newImprovement = Improvement("data/images/moreLife.png", 'life', objectGroup, improvementGroup)
 
-        # Colisão de jogador com inimigo encerra game
-        collisions = pygame.sprite.spritecollide(player, enemyGroup, False)
+        enemyCollisions = pygame.sprite.spritecollide(player, enemyGroup, True)
 
-        if collisions:
-            sounds.play('fah')
-            time.sleep(3)
-            gameLoop = False
+        if enemyCollisions:
+            if remainingLifes == 1:
+                sounds.play('fah')
+                time.sleep(3)
+                gameLoop = False
+            else:
+                remainingLifes -= 1
+                quantityLifes.update_text(f": {remainingLifes}")
 
-        # Disparo do jogador destrói inimigo
+        improvementCollisions = pygame.sprite.spritecollide(player, improvementGroup, True)
+
+        for improvement in improvementCollisions:
+            if improvement.tipo == 'life':
+                remainingLifes += 1
+                quantityLifes.update_text(f": {remainingLifes}")
+                sounds.play('up')
+            elif improvement.tipo == 'shot':
+                numberShots += 1
+                sounds.play('up')
 
         hits = pygame.sprite.groupcollide(shotGroup, enemyGroup, True, True)
 
-        draw()
+
         pygame.display.update()
