969aa1f7df33b03b622223e524e0c6e2fbb0ae5f
Elton Luiz <eltonluiz533@gmail.com>
2026-03-23 00:55:14 -0300

    feat: ajusta movimentação do jogador, agora é possível se movimentar em todas as direções e melhorias na acelaração de movimento

--- player.py
+++ player.py
@@ -1,22 +1,24 @@
 import pygame
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, groups, imagePath, rectPlayer, numberPlayer):
+    def __init__(self, groups, imagePath, numberPlayer):
         super().__init__(groups)
 
         # Carrega imagem e dimensões do jogador
         self.image = pygame.image.load(imagePath)
-        self.image = pygame.transform.scale(self.image, [100, 100])
+        self.image = pygame.transform.scale(self.image, [60, 60])
 
         # Recebe posição inicial
 
-        self.rect = pygame.Rect(rectPlayer)
+        self.rect = pygame.Rect(400,450,60,60);
 
         self.numberPlayer = numberPlayer
 
         # Variáveis
-        self.speed = 0
-        self.acceleration = 0.1
+        self.xspeed = 0
+        self.xacceleration = 0.2
+        self.yspeed = 0
+        self.yacceleration = 0.2
 
     def update(self, *args):
 
@@ -27,29 +29,49 @@ class Player(pygame.sprite.Sprite):
             # Movimentação do jogador com o uso de aceleração
 
             if keys[pygame.K_d]:
-                self.speed += self.acceleration
+                self.xspeed += self.xacceleration
             elif keys[pygame.K_a]:
-                self.speed -= self.acceleration
+                self.xspeed -= self.xacceleration
             elif keys[pygame.K_RIGHT]:
-                self.speed += self.acceleration
+                self.xspeed += self.xacceleration
             elif keys[pygame.K_LEFT]:
-                self.speed -= self.acceleration
+                self.xspeed -= self.xacceleration
             else:
-                self.speed *= 0.95
+                self.xspeed *= 0.95
 
+            if keys[pygame.K_w]:
+                self.yspeed -= self.yacceleration
+            elif keys[pygame.K_s]:
+                self.yspeed += self.yacceleration
+            elif keys[pygame.K_UP]:
+                self.yspeed -= self.yacceleration
+            elif keys[pygame.K_DOWN]:
+                self.yspeed += self.yacceleration
+            else:
+                self.yspeed *= 0.95
 
-            self.rect.x += self.speed
+            if self.xspeed > 7:
+                self.xspeed = 7
+            if self.yspeed > 7:
+                self.yspeed = 7
+
+            self.rect.x += self.xspeed
+            self.rect.y += self.yspeed
 
         # Validações para impedir o jogador de sair da tela
         if self.rect.top < 0:
             self.rect.top = 0
-            self.speed = 0
+            self.xspeed = 0
+            self.yspeed = 0
         if self.rect.bottom > 600:
             self.rect.bottom = 600
-            self.speed = 0
+            self.xspeed = 0
+            self.yspeed = 0
         if self.rect.left < 0:
             self.rect.left = 0
-            self.speed = 0
+            self.xspeed = 0
+            self.yspeed = 0
         if self.rect.right > 800:
             self.rect.right = 800
-            self.speed = 0
+            self.xspeed = 0
+            self.yspeed = 0
