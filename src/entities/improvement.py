a9e52ad1038e8c49a26db274a8a31141d94a67d1
Elton Luiz <eltonluiz533@gmail.com>
2026-04-03 11:41:04 -0300

    feat: classe de aprimoramento agora pode receber um rect, permitindo ao eliminar um inimigo fazer surgir o aprimoramento do local onde ele foi morto

--- src/entities/improvement.py
+++ src/entities/improvement.py
@@ -3,7 +3,7 @@ import random
 from ..core.settings import Settings
 
 class Improvement(pygame.sprite.Sprite):
-    def __init__(self, imagePath, tipo,*groups):
+    def __init__(self, imagePath, tipo, improvementRect,*groups):
         super().__init__(*groups)
 
         self.settings = Settings()
@@ -12,12 +12,19 @@ class Improvement(pygame.sprite.Sprite):
         self.image = pygame.image.load(imagePath)
         self.image = pygame.transform.scale(self.image, [40, 40])
 
+        if improvementRect == None:
         # Posição inicial
-        self.rect = pygame.Rect(0, 0, 100, 100)
+            self.rect = pygame.Rect(0, 0, 100, 100)
 
-        # Surgimento aleatório no mapa
-        self.rect.y = random.randint(-400,-1)
-        self.rect.x = random.randint(1,self.settings.WIDTH - 100)
+            # Surgimento aleatório no mapa
+            self.rect.y = random.randint(-400,-1)
+            self.rect.x = random.randint(1,self.settings.WIDTH - 100)
+
+        else:
+            self.rect = pygame.Rect(0, 0, 100, 100)
+
+            self.rect.y = improvementRect.y
+            self.rect.x = improvementRect.x
 
         self.tipo = tipo
 
