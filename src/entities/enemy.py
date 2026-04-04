0a09e914edb025a13c4c8ece5d8da242fd83ba76
Elton Luiz <eltonluiz533@gmail.com>
2026-04-03 21:45:19 -0300

    fix: ajusta movimentação do asteroid ao sair da tela

--- src/entities/enemy.py
+++ src/entities/enemy.py
@@ -9,18 +9,18 @@ class Enemy(pygame.sprite.Sprite):
         self.settings = Settings()
 
         # Tamanho aleatório para o asteroide
-        size = random.randint(40,120)
+        self.size = random.randint(40,120)
 
         # Carregando imagem de forma dinâmica
         self.image = pygame.image.load(imagePath)
-        self.image = pygame.transform.scale(self.image, [size, size])
+        self.image = pygame.transform.scale(self.image, [self.size, self.size])
 
         # self.rect = pygame.Rect(enemyRect)
-        self.rect = pygame.Rect(0, 0, size, size)
+        self.rect = pygame.Rect(0, 0, self.size, self.size)
 
         # Surgimento aleatório no mapa
         self.rect.y = random.randint(-400,-1)
-        self.rect.x = random.randint(1,self.settings.WIDTH - size)
+        self.rect.x = random.randint(1,self.settings.WIDTH - self.size)
 
         # Velociade aleatória para cada asteroide / inimigo
         self.speed = 1 + random.random() * 2
@@ -30,5 +30,5 @@ class Enemy(pygame.sprite.Sprite):
         self.rect.y += self.speed
 
         # Após passar do limite da tela é destruído
-        if self.rect.bottom > self.settings.HEIGHT:
+        if self.rect.bottom > self.settings.HEIGHT + self.size:
             self.kill()
